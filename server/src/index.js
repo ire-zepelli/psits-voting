3df8b0bfa2def7800161eb477dd7814963852b85
Benedict <138974722+ire-zepelli@users.noreply.github.com>
2025-10-06 20:48:47 +0800

    added dependencies

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
